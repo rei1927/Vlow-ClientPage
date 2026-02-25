098034707d500427506f1d557ae05bcbda2073e3
Reiza rachmattullah <reizarachmattullah@Reizas-Mac-mini.local>
2026-02-25 13:05:26 +0700

    fix: increase body parser limit to 10mb for file upload support

--- server/server.js
+++ server/server.js
@@ -48,8 +48,8 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ extended: true, limit: "2mb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
