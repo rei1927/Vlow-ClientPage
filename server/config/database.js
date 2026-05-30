b5fb96486533547d8ace90eefd25630724a72c10
Reiza rachmattullah <reizarachmattullah@Reizas-Mac-mini.local>
2026-05-31 00:17:38 +0700

    fix: enforce DB fallback values in sequelize if environment variables are empty

--- server/config/database.js
+++ server/config/database.js
@@ -4,8 +4,12 @@ import logger from "../utils/logger.js";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "vlow_db",
+  process.env.DB_USER || "vlow",
+  process.env.DB_PASS || "vlow_secret",
+  {
+  host: process.env.DB_HOST || "postgres",
   dialect: "postgres", // Explicitly set for production
   logging: (msg) => logger.debug(msg),
   dialectOptions: {
